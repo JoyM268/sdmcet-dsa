8cbd3891afb663147c05f75ad70d27e9c5d6d65d
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-09-26 20:12:27 +0530

    Create palindromeStringStack.c

---
+++ Extras/palindromeStringStack.c
@@ -0,0 +1,67 @@
+//Check if a string is palindrome using stack
+#include<stdio.h>
+#include<string.h>
+#define SIZE 50
+
+struct stack{
+    int stk[SIZE];
+    int top;
+} s;
+
+void push(int);
+int pop(void);
+int isPalindrome(char *); 
+main()
+{
+    s.top = -1;
+    char str[SIZE];
+    printf("Enter the input string: ");
+    scanf("%[^\n]", str);
+    if(isPalindrome(str))
+    {
+        printf("%s is a palindrome\n", str);
+    }
+    else
+    {
+        printf("%s is not a palindrome\n", str);
+    }
+}
+
+void push(int item)
+{
+    if(s.top == SIZE - 1)
+    {
+        printf("Stack overflow\n");
+        return;
+    }
+    s.stk[++s.top] = item;
+}
+
+int pop()
+{
+    if(s.top == -1)
+    {
+        printf("Stack underflow\n");
+        return -1;
+    }
+    return s.stk[s.top--];
+}
+
+int isPalindrome(char *str)
+{
+    int len = strlen(str), i;
+    for(i = 0; i < len; i++)
+    {
+        push(str[i]);
+    }
+    i = 0;
+    while(s.top != -1)
+    {
+        if(str[i] != pop())
+        {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}

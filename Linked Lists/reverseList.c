ea259ba85ed0f540adc60bdbf707de575c92fcbf
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-11-15 18:57:01 +0530

    Create File

---
+++ Linked Lists/reverseList.c
@@ -0,0 +1,100 @@
+//Write a C program to reverse a linked list
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int info;
+    struct node *link;
+};
+
+typedef struct node *NODE;
+
+NODE createNode()
+{
+    NODE ptr = (NODE)malloc(sizeof(struct node));
+    return ptr;
+}
+
+NODE reverseSLL(NODE first)
+{
+    NODE rev = NULL, temp;
+    while(first != NULL)
+    {
+        temp = first;
+        first = first -> link;
+        temp -> link = rev;
+        rev = temp;
+    }
+    return rev;
+}
+
+NODE insertRearSLL(NODE first, int ele)
+{
+    NODE temp = createNode();
+    temp -> info = ele;
+    temp -> link = NULL;
+    if(first == NULL)
+    {
+        return temp;
+    }
+    else
+    {
+        NODE cur = first;
+        while (cur->link != NULL)
+        {
+            cur = cur->link;
+        }
+        cur->link = temp;
+        return first;
+    }
+}
+
+void display(NODE first)
+{
+    if(first == NULL)
+    {
+        printf("List is empty\n");
+        return;
+    }
+    else
+    {
+        NODE cur = first;
+        while(cur != NULL)
+        {
+            printf("%d --> ", cur -> info);
+            cur = cur -> link;
+        }
+        printf("NULL\n");
+    }
+}
+
+int main()
+{
+    NODE first = NULL;
+    int item, choice;
+    for(;;)
+    {
+        printf("\n1.Insert at rear\t2.Reverse the list\t3.Display\t4.Exit\n");
+        printf("Enter the choice: ");
+        scanf("%d", &choice);
+        switch(choice)
+        {
+            case 1:
+                printf("Enter the element to be inserted: ");
+                scanf("%d", &item);
+                first = insertRearSLL(first, item);
+                break;
+            case 2:
+                first = reverseSLL(first);
+                break;
+            case 3:
+                display(first);
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+    return 0;
+}

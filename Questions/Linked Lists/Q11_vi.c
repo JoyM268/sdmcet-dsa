66e0ebedeac575e2c858536815be157dcb48ca79
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-12-11 20:48:20 +0530

    Add File

---
+++ Questions/Linked Lists/Q11_vi.c
@@ -0,0 +1,138 @@
+/* Write a C function search(l,x) that accepts a pointer 'l' to a list of integers and an integer x and returns a pointer to a node containing x, if it exists
+or the null pointer otherwise. Write another function srchinsrt(l,x), that adds 'x' to 'l' if it is not found and always returns a pointer to a node containing 
+'x'. */
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int info;
+    struct node *link;
+};
+typedef struct node *NODE;
+
+NODE createNode()
+{
+    NODE ptr = (NODE)malloc(sizeof(struct node));
+    return ptr;
+}
+
+NODE insertAtEnd(NODE first, int ele)
+{
+    NODE temp = createNode();
+    temp -> info = ele;
+    temp -> link = NULL;
+    if(first == NULL)
+    {
+        return temp;
+    }
+    else
+    {
+        NODE cur = first;
+        while(cur -> link != NULL)
+        {
+            cur = cur -> link;
+        }
+        cur -> link = temp;
+        return first;
+    }
+}
+
+void displayList(NODE first)
+{
+    if(first == NULL)
+    {
+        printf("List is empty\n");
+        return;
+    }
+    else
+    {
+        NODE cur = first;
+        while(cur != NULL)
+        {
+            printf("%d --> ", cur -> info);
+            cur = cur -> link;
+        }
+        printf("NULL\n");
+    }
+}
+
+NODE search(NODE l, int x)
+{
+    while(l != NULL)
+    {
+        if(l -> info == x)
+            return l;
+        l = l -> link;
+    }
+    return NULL;
+}
+
+NODE searchInsert(NODE *l, int x)
+{
+    NODE node = search(*l, x);
+    if(node)
+    {
+        printf("%d exists in the list\n", node -> info);
+        return node;
+    }
+    NODE temp = *l, newNode = createNode();
+    newNode -> info = x;
+    newNode -> link = NULL;
+    if(temp == NULL)
+    {
+        *l = newNode;
+        return newNode;
+    }
+    while(temp -> link != NULL)
+    {
+        temp = temp -> link;
+    }
+    temp -> link = newNode;
+    printf("%d inserted into the list\n", newNode -> info);
+    return newNode;
+}
+
+int main()
+{
+    NODE first = NULL, xs;
+    int choice, item;
+    for(;;)
+    {
+        printf("\n1:Insert in list\t2:Display list\t3:Search Element\t4:Insert Search\t5:Exit\n");
+        printf("Enter the choice(1, 2, 3, 4): ");
+        scanf("%d", &choice);
+        switch(choice)
+        {
+            case 1:
+                printf("Enter the element to be inserted: ");
+                scanf("%d", &item);
+                first = insertAtEnd(first, item);
+                break;
+            case 2:
+                displayList(first);
+                break;
+            case 3:
+                printf("Enter the key element: ");
+                scanf("%d", &item);
+                if(search(first, item))
+                {
+                    printf("%d found\n", item);
+                }
+                else
+                {
+                    printf("%d not found\n", item);
+                }
+                break;
+            case 4:
+                printf("Enter the key element: ");
+                scanf("%d", &item);
+                xs = searchInsert(&first, item);
+                break;
+            case 5:
+                exit(0);
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+    return 0;
+}

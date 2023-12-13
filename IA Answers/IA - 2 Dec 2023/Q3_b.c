3f5adb29442da046833b38bafb3cf81fbd6d44c9
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-12-13 19:12:03 +0530

    Add File

---
+++ IA Answers/IA - 2 Dec 2023/Q3_b.c
@@ -0,0 +1,16 @@
+//Q3)b) Construct the Binary Search Tree for the given data. 14, 15, 4, 9, 7, 18, 3, 5, 16, 20, 17. Write inorder, preorder and postorder trversal 
+//of the constructed tree
+
+            14
+           /  \
+          4    15
+         / \     \
+        3   9     18
+           /     /  \
+          7     16  20
+         /        \
+        5          17
+
+Inorder: 3, 4, 5, 7, 9, 14, 15, 16, 17, 18, 20
+Preorder: 14, 4, 3, 9, 7, 5, 15, 18, 16, 17, 20
+Postorder: 3, 5, 7, 9, 4, 17, 16, 20, 18, 15, 14

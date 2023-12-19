b7e2e2ff4b08257a2a02ab6b63b18df074b4fb50
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-12-19 18:56:32 +0530

    Modify File

--- Trees - 1/bstInsertion.c
+++ Trees - 1/bstInsertion.c
@@ -1,75 +1,76 @@
-//C Program to insert a element in a binary search tree
-#include<stdio.h>
-#include<stdlib.h>
-struct node {
-    int info;
-    struct node *left, *right;
+// C Program to insert a element in a binary search tree
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+	int info;
+	struct node *left, *right;
 };
 typedef struct node *NODE;
 
 NODE createNode()
 {
-  NODE root = (NODE)malloc(sizeof(struct node));
-  return root;
+	NODE root = (NODE)malloc(sizeof(struct node));
+	return root;
 }
 
 NODE insertBst(NODE root, int ele)
 {
-  NODE cur, prev;
-  NODE temp = createNode();
-  temp -> info = ele;
-  temp -> left = temp -> right = NULL;
-  if(root == NULL)
-	return temp;
-  cur = root, prev = NULL;
-  while(cur != NULL)
-  {
-	prev = cur;
-	if(ele < cur -> info)
-	  cur = cur -> left;
-    else
-	  cur = cur -> right;
-  }
-  if(ele < prev -> info)
-    prev -> left = temp;
-  else
-	prev -> right = temp;
-  return root;
+	NODE cur, prev;
+	NODE temp = createNode();
+	temp->info = ele;
+	temp->left = temp->right = NULL;
+	if (root == NULL)
+		return temp;
+	cur = root, prev = NULL;
+	while (cur != NULL)
+	{
+		prev = cur;
+		if (ele < cur->info)
+			cur = cur->left;
+		else
+			cur = cur->right;
+	}
+	if (ele < prev->info)
+		prev->left = temp;
+	else
+		prev->right = temp;
+	return root;
 }
 
 void inorderBst(NODE root)
 {
-	if(root == NULL)
+	if (root == NULL)
 		return;
-	inorderBst(root -> left);
-	printf("%d ", root -> info);
-	inorderBst(root -> right);
+	inorderBst(root->left);
+	printf("%d ", root->info);
+	inorderBst(root->right);
 }
 
 int main()
 {
 	NODE root = NULL;
 	int item, choice;
-	for(;;)
+	for (;;)
 	{
 		printf("\n1.Insert\t2.Inorder print\t3.Exit\n");
 		printf("Enter the choice: ");
 		scanf("%d", &choice);
-		switch(choice)
+		switch (choice)
 		{
-			case 1:
-				printf("Enter the element to be inserted: ");
-				scanf("%d", &item);
-				root = insertBst(root, item);
-				break;
-			case 2:
-				inorderBst(root);
-				printf("\n");
-				break;
-			case 3:
-				exit(0);
-			default:
-				printf("Invalid choice\n");
+		case 1:
+			printf("Enter the element to be inserted: ");
+			scanf("%d", &item);
+			root = insertBst(root, item);
+			break;
+		case 2:
+			inorderBst(root);
+			printf("\n");
+			break;
+		case 3:
+			exit(0);
+		default:
+			printf("Invalid choice\n");
 		}
 	}
 }

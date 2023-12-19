45db13a3515a090a35411bff2b3271e6f9eb52db
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-12-19 19:04:42 +0530

    Modify File

--- Trees - 1/bstInsertion.c
+++ Trees - 1/bstInsertion.c
@@ -28,7 +28,7 @@ NODE insertBst(NODE root, int ele)
         if(ele < cur -> info)
             cur = cur -> left;
         else
-	        cur = cur -> right;
+            cur = cur -> right;
     }
     if(ele < prev -> info)
         prev -> left = temp;
@@ -39,37 +39,37 @@ NODE insertBst(NODE root, int ele)
 
 void inorderBst(NODE root)
 {
-	if(root == NULL)
-		return;
-	inorderBst(root -> left);
-	printf("%d ", root -> info);
-	inorderBst(root -> right);
+    if(root == NULL)
+        return;
+    inorderBst(root -> left);
+    printf("%d ", root -> info);
+    inorderBst(root -> right);
 }
 
 int main()
 {
     NODE root = NULL;
     int item, choice;
-	for(;;)
-	{
-		printf("\n1.Insert\t2.Inorder print\t3.Exit\n");
-		printf("Enter the choice: ");
-		scanf("%d", &choice);
-		switch(choice)
-		{
-			case 1:
-				printf("Enter the element to be inserted: ");
-				scanf("%d", &item);
-				root = insertBst(root, item);
-				break;
-			case 2:
-				inorderBst(root);
-				printf("\n");
-				break;
-			case 3:
-				exit(0);
-			default:
-				printf("Invalid choice\n");
-		}
-	}
+    for(;;)
+    {
+        printf("\n1.Insert\t2.Inorder print\t3.Exit\n");
+        printf("Enter the choice: ");
+        scanf("%d", &choice);
+        switch(choice)
+        {
+            case 1:
+                printf("Enter the element to be inserted: ");
+                scanf("%d", &item);
+                root = insertBst(root, item);
+                break;
+            case 2:
+                inorderBst(root);
+                printf("\n");
+                break;
+            case 3:
+                exit(0);
+            default:
+                printf("Invalid choice\n");
+        }
+    }
 }
